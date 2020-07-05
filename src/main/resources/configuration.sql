e82abd9398c6bc4abdca0828f75f0bfe695cf605
Wanchalerm Yuphasuk <enjizen.r@gmail.com>
2020-07-06 01:08:16 +0700

    Update sql

--- src/main/resources/configuration.sql
+++ src/main/resources/configuration.sql
@@ -1,54 +1,17 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
---
--- Host: 127.0.0.1    Database: configuration
--- ------------------------------------------------------
--- Server version	5.7.23
+create database configuration;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `properties`
---
+use configuration;
 
 DROP TABLE IF EXISTS `properties`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `properties` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `application` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `profile` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `key` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `value` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  application VARCHAR(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profile` VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `key` VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `value` VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `properties`
---
-
-LOCK TABLES `properties` WRITE;
-/*!40000 ALTER TABLE `properties` DISABLE KEYS */;
-INSERT INTO `properties` VALUES (1,'car','default','server.servlet.contextPath','/car'),(2,'car','default','server.port','8090'),(3,'car','default','spring.datasource.url','jdbc:mysql://127.0.0.1:3306/car?currentSchema=car'),(4,'car','default','spring.datasource.username','root'),(5,'car','default','spring.datasource.password','P@ssw0rd'),(6,'car','default','spring.jpa.generate-ddl','true');
-/*!40000 ALTER TABLE `properties` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-10-27  3:10:35
+ALTER TABLE `configuration`.`properties`
+ADD INDEX `get configuration` (`application` ASC, `profile` ASC);
+;
